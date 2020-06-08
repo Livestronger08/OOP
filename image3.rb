8def1c9e8c2db5ad1e29194c0cdcb1e8dc0a0433
Livestronger08 <derek.downie@ttu.edu>
2020-06-08 17:31:14 +0000

    Image3: issues with the end

--- image3.rb
+++ image3.rb
@@ -1,65 +1,59 @@
-class Image
-    def initialize(image)
-      @image = image
-    end
-  
-    def find_ones
-      list_of_ones = [] #create empty array to track found 1's
-  
-        @image.each_with_index do |row, row_number|
-          row.each_with_index do |item, col_number|
-  
-            if item == 1
-              list_of_ones.push( [row_number, col_number] )
-            end
-          end
-        end
-        list_of_ones
-    end
-    
-    def output_image
-      @image.each do |image|
-        puts image.join(' ')
-      end
-    end
-  
-    def blur
-      list_of_ones = find_ones
-      @image.each_with_index do |row, row_number|
-        row.each_with_index do |item, col_number|
-          list_of_ones.each do |found_row_number, found_col_number|
-            if row_number == found_row_number && col_number == found_col_number
-              @image[row_number -1][col_number] = 1 unless row_number == 0
-              @image[row_number +1][col_number] = 1 unless row_number == 0
-              @image[row_number][col_number -1] = 1 unless col_number == 0
-              @image[row_number][col_number +1] = 1 unless col_number == 0
-              
-            end
-          end 
-        end
-      end
-    end 
-  end
-  
-  image1 = Image.new([
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-  ])
-  
-  image2 = Image.new([
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-  ])
+Class Image
 
-  image3 = Image.new([
+    def initialize(image)
+        @image = image
+    end
+
+    def blur_man(distance)
+        distance.times do
+            find_ones
+        end
+        manhattan_image
+    end
+
+    def blur
+        if row_number != 0
+            @image[row_number - 1][col_number] = 1
+        end
+        if row_number != image.length - 1
+            @image[row_number + 1][col_number] = 1
+        end
+        if col_number != 0
+            @image[row_number][col_number - 1] = 1
+        end
+
+        first_row = image[0]
+        if col_number != first_row.length - 1
+            @image[row_number][col_number + 1] = 1
+        end
+    end
+
+    def find_ones
+        ones = []
+        @image.each_with_index do |row, row_number|
+            row.each_with_index do |item, col_number|
+                if item == 1
+                    ones.push [row_number, col_number]
+                end
+            end
+        end
+            ones.each do |pair|
+            row_number, col_number = pair
+            blur(row_number, col_number)
+        end
+    end
+
+    def manhattan_image
+        @image.each do |row|
+            row.each do |item|
+                print item
+            end
+            puts
+        end
+    end
+end
+
+image = Image.new([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -71,13 +65,6 @@ class Image
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 1]
-  ])
+])
 
-  image1.blur
-  image1.output_image
-  
-  image2.blur(2)
-  image2.output_image
-  
-  image3.blur
-  image3.output_image 
+image.blur_man(3)
